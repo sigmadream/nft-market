c389b824c8c88b612753962f3f00fe41eea50eca
Sangkon Han <sigmadream@gmail.com>
2022-09-11 04:51:50 +0900

    //wip
    added js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-const Home = () => {
-  <div>
+const Home = () => (
+  < div >
     <h1 className="text-3xl font-bold underline">
       Hello, World!
     </h1>
-  </div>;
-};
+  </div >
+);
 
 export default Home;
---
+++ pages/my-nfts.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const MyNFTs = () => (
+    <div>MyNFTs</div>
+);
+
+export default MyNFTs;
