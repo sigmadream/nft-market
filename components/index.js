643c8b3fdd64fb197f3da562f34177da3d2f8bef
Sangkon Han <sigmadream@gmail.com>
2022-09-22 02:11:57 +0900

    //wip:topseller

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import Button from './Button';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Footer, Navbar, Button, Banner };
+export { Footer, Navbar, Button, Banner, CreatorCard };
