42135bdd72cf0efb2557e62ce1461f7c30d0d55c
Sangkon, Han <sigmadream@gmail.com>
2022-10-06 02:43:05 +0900

    //wip

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -29,56 +29,56 @@ const CreateNFT = () => {
                 <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Create new NFT</h1>
                 <div className="mt-16">
                     <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload File</p>
-                </div>
-                <div className="mt-4">
-                    <div {...getRootProps()} className={fileStyle}>
-                        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">JPG, PNG, GIF, SVG, WEBM Max 100mb.</p>
-                        <div className="my-12 w-full flex justify-center">
-                            <Image
-                                src={images.upload}
-                                width={100}
-                                height={100}
-                                objectFit="contain"
-                                alt="file upload"
-                                className={theme === 'light' ? 'filter invert' : ''}
-                            />
+                    <div className="mt-4">
+                        <div {...getRootProps()} className={fileStyle}>
+                            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">JPG, PNG, GIF, SVG, WEBM Max 100mb.</p>
+                            <div className="my-12 w-full flex justify-center">
+                                <Image
+                                    src={images.upload}
+                                    width={100}
+                                    height={100}
+                                    objectFit="contain"
+                                    alt="file upload"
+                                    className={theme === 'light' ? 'filter invert' : ''}
+                                />
+                            </div>
+                            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">Drag and Drop File</p>
+                            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">or Browse media on your device</p>
                         </div>
-                        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">Drag and Drop File</p>
-                        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">or Browse media on your device</p>
                     </div>
+                    {fileUrl && (
+                        <aside>
+                            <div>
+                                <img src={fileUrl} alt="asset_file" />
+                            </div>
+                        </aside>
+                    )}
                 </div>
-                {fileUrl && (
-                    <aside>
-                        <div>
-                            <img src={fileUrl} alt="asset_file" />
-                        </div>
-                    </aside>
-                )}
-            </div>
-            <Input
-                inputType="input"
-                title="Name"
-                placeholder="NFT Name"
-                handleClick={() => { }}
-            />
-            <Input
-                inputType="textarea"
-                title="Description"
-                placeholder="NFT Description"
-                handleClick={() => { }}
-            />
-            <Input
-                inputType="number"
-                title="Price"
-                placeholder="NFT Price"
-                handleClick={() => { }}
-            />
-            <div className="mt-10 w-full flex justify-center">
-                <Button
-                    btnName="Create NFT"
-                    classStyles="rounded-xl"
+                <Input
+                    inputType="input"
+                    title="Name"
+                    placeholder="NFT Name"
                     handleClick={() => { }}
                 />
+                <Input
+                    inputType="textarea"
+                    title="Description"
+                    placeholder="NFT Description"
+                    handleClick={() => { }}
+                />
+                <Input
+                    inputType="number"
+                    title="Price"
+                    placeholder="NFT Price"
+                    handleClick={() => { }}
+                />
+                <div className="mt-10 w-full flex justify-center">
+                    <Button
+                        btnName="Create NFT"
+                        classStyles="rounded-xl"
+                        handleClick={() => { }}
+                    />
+                </div>
             </div>
         </div>
     );
