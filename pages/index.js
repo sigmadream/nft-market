5584c45f1f9515ec63f0683d36174db36ecc16b4
Sangkon, Han <sigmadream@gmail.com>
2022-10-06 01:47:05 +0900

    //wip:nft-temp

--- pages/index.js
+++ pages/index.js
@@ -80,6 +80,19 @@ const Home = () => {
             </div>
           </div>
         </div>
+        <div className="mt-10">
+          <div className="flexBetween mx-4 sm:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot NFTs</h1>
+            <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+              <div>
+                SearchBar
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (console.log(i)))}
+          </div>
+        </div>
       </div>
     </div>
   );
