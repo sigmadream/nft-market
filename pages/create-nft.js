4e8dba46a51f38e9145105c26db98e093c5ad614
Sangkon, Han <sigmadream@gmail.com>
2022-10-06 02:16:42 +0900

    //wip:layout

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,7 +1,18 @@
-import react from "react";
+import React, { useState, useMemo, useCallback, useContext } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import images from '../assets';
 
-const CreateNFT = () => (
-    <div>CreateNFT</div>
-);
+
+const CreateNFT = () => {
+    return (
+        <div className="flex justify-center sm:px-4 p-12">
+            <div className="w-3/5 md:w-full">
+                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Create new NFT</h1>
+            </div>
+        </div>
+    );
+};
 
 export default CreateNFT;
